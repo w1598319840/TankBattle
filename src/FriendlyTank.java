991449e8c5fda7d917cb94f58e1c0e0acd33660c
w159_ <1598319840@qq.com>
2024-01-12 19:38:00 +0800

    TankFight---Version 1.2 (2024/1/12)

--- src/FriendlyTank.java
+++ src/FriendlyTank.java
@@ -1,5 +1,5 @@
 public class FriendlyTank extends Tank {
-    public FriendlyTank(int x, int y) {
-        super(x, y);
+    public FriendlyTank(int x, int y, int direction, int type) {
+        super(x, y, direction, type);
     }
 }
--- src/Tank.java
+++ src/Tank.java
@@ -1,10 +1,12 @@
 public class Tank {
     //作为所有坦克(包括友方坦克和地方坦克)的父类
-    private int x, y;//坦克的坐标
+    private int x, y, direction, type;//坦克的坐标
 
-    public Tank(int x, int y) {
+    public Tank(int x, int y, int direction, int type) {
         this.x = x;
         this.y = y;
+        this.direction = direction;
+        this.type = type;
     }
 
     public int getX() {
@@ -21,5 +23,21 @@ public class Tank {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 }
